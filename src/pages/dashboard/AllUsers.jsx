5bb8631363c4470b91fd9dff00e26c031041636d
mjh-shikder <mjhsikder@gmail.com>
2025-12-16 19:32:52 +0600

    user status change function added

--- src/pages/dashboard/AllUsers.jsx
+++ src/pages/dashboard/AllUsers.jsx
@@ -4,90 +4,108 @@ import useAxiosSecure from "../../hooks/useAxiosSecure";
 const AllUsers = () => {
   const axiosSecure = useAxiosSecure();
 
-  const [allUsers, setAllUsers] = useState([]);
+    const [allUsers, setAllUsers] = useState([]);
+    
+    const fetchUsers = () => {
+        axiosSecure.get("/users").then((res) => {
+          setAllUsers(res.data);
+        });
+    }
 
   useEffect(() => {
-    axiosSecure.get("/users").then((res) => {
-      setAllUsers(res.data);
-    });
+    fetchUsers()
   }, [axiosSecure]);
 
   console.log(allUsers);
 
-    return (
-      <div>
-        <div className="overflow-x-auto">
-          <table className="table">
-            {/* head */}
-            <thead>
-              <tr>
-                <th>Name & Role</th>
-                <th>Contact & Location</th>
+  const handleStatusChange = (email, status) => {
+    axiosSecure
+      .patch(`/update/user/status?email=${email}&status=${status}`)
+      .then((res) => {
+          console.log(res.data);
+          fetchUsers();
+      });
+  };
 
-                <th>Status</th>
-                <th>Action</th>
-              </tr>
-            </thead>
-            <tbody>
-              {/* row 1 */}
-              {allUsers.map((user) => (
-                <tr key={user._id}>
-                  {/* <th>
+  return (
+    <div>
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>Name & Role</th>
+              <th>Contact & Location</th>
+
+              <th>Status</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* row 1 */}
+            {allUsers.map((user) => (
+              <tr key={user._id}>
+                {/* <th>
                     <label>
                       <input type="checkbox" className="checkbox" />
                     </label>
                   </th> */}
-                  <td>
-                    <div className="flex items-center gap-3">
-                      <div className="avatar">
-                        <div className="mask mask-squircle h-12 w-12">
-                          <img
-                            src={user?.mainPhotoUrl}
-                            alt="Avatar Tailwind CSS Component"
-                          />
-                        </div>
-                      </div>
-                      <div>
-                        <div className="font-bold">{user?.name}</div>
-                        <div className="text-sm opacity-50">{user?.role}</div>
+                <td>
+                  <div className="flex items-center gap-3">
+                    <div className="avatar">
+                      <div className="mask mask-squircle h-12 w-12">
+                        <img
+                          src={user?.mainPhotoUrl}
+                          alt="Avatar Tailwind CSS Component"
+                        />
                       </div>
                     </div>
-                  </td>
-                  <td>
-                    Email: {user?.email}
-
-                    <br />
-                    <p className="badge badge-ghost badge-sm">
-                      Location: {user?.district}, {user?.upazila}
-                    </p>
-                  </td>
-                  <td
-                    className={
-                      user?.status == "Active"
-                        ? "text-accent font-semibold"
-                        : "text-primary font-semibold"
-                    }
-                  >
-                    {user?.status}
-                  </td>
-                  <th>
-                    {user?.status == "Active" ? (
-                      <button className="btn btn-primary text-white btn-xs">
-                        Block
-                      </button>
-                    ) : (
-                      <button className="btn btn-accent text-white btn-xs">
-                        Active
-                      </button>
-                    )}
-                  </th>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
+                    <div>
+                      <div className="font-bold">{user?.name}</div>
+                      <div className="text-sm opacity-50">{user?.role}</div>
+                    </div>
+                  </div>
+                </td>
+                <td>
+                  Email: {user?.email}
+                  <br />
+                  <p className="badge badge-ghost badge-sm">
+                    Location: {user?.district}, {user?.upazila}
+                  </p>
+                </td>
+                <td
+                  className={
+                    user?.status == "Active"
+                      ? "text-accent font-semibold"
+                      : "text-primary font-semibold"
+                  }
+                >
+                  {user?.status}
+                </td>
+                <th>
+                  {user?.status == "Active" ? (
+                    <button
+                      onClick={() => handleStatusChange(user?.email, "Blocked")}
+                      className="btn btn-primary text-white btn-xs"
+                    >
+                      Block
+                    </button>
+                  ) : (
+                    <button
+                      onClick={() => handleStatusChange(user?.email, "Active")}
+                      className="btn btn-accent text-white btn-xs"
+                    >
+                      Active
+                    </button>
+                  )}
+                </th>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
-    );
+    </div>
+  );
 };
 
 export default AllUsers;
