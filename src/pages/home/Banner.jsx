ade763795467b808d05ad575a636ea63b935b909
mjh-shikder <mjhsikder@gmail.com>
2026-02-28 04:16:20 +0600

    Banner Link updated

--- src/pages/home/Banner.jsx
+++ src/pages/home/Banner.jsx
@@ -20,18 +20,40 @@ const Banner = () => {
           className="h-125   "
         >
           <SwiperSlide className=" ">
-            <div className=" flex justify-center items-center w-full h-full bg-no-repeat bg-center bg-[url(https://images.unsplash.com/photo-1683791895200-201c0c40310f?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] ">
+            {" "}
+            <div className=" flex justify-center items-center w-full h-full bg-cover  bg-[url(https://images.unsplash.com/photo-1615461065624-21b562ee5566?q=80&w=1340&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] ">
               <div
                 className="flex flex-col items-center space-y-5 
-      backdrop-blur-xl rounded-xl brightness-90 saturate-120 px-10 py-8"
+      backdrop-blur-md rounded-xl brightness-90 saturate-120 px-10 py-8"
               >
-                <h1 className=" md:text-5xl text-3xl text-gray-800  font-semibold">
-                  Be Someone's Hero Today
+                <h1 className=" md:text-5xl text-3xl text-white font-semibold">
+                  Need Emergency Blood?
                 </h1>
-                <p className="text-gray-800 ">Your Blood Can Save a Life</p>
+                <p className="text-white ">Find The Donators Near you</p>
                 <div className="flex gap-2.5 md:gap-5">
                   <Link
-                    className="btn  rounded-xl btn-accent btn-lg text-white"
+                    className="btn rounded-xl btn-primary btn-lg text-white"
+                    to={"/dashboard/create-donation-request"}
+                  >
+                    Request Donation
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide className=" ">
+            <div className=" flex justify-center items-center w-full h-full bg-cover bg-center bg-[url(https://images.unsplash.com/photo-1733119883210-04f09d5f86df?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] ">
+              <div
+                className="flex flex-col items-center space-y-5 
+      backdrop-blur-md rounded-xl brightness-90 saturate-120 px-10 py-8"
+              >
+                <h1 className=" md:text-5xl text-3xl text-white  font-semibold">
+                  Be Someone's Hero Today
+                </h1>
+                <p className="text-white ">Your Blood Can Save a Life</p>
+                <div className="flex gap-2.5 md:gap-5">
+                  <Link
+                    className="btn  rounded-xl btn-primary btn-lg text-white"
                     to={"/register"}
                   >
                     Become a Donor
@@ -42,34 +64,12 @@ const Banner = () => {
           </SwiperSlide>
           <SwiperSlide className=" ">
             {" "}
-            <div className=" flex justify-center items-center w-full h-full bg-cover  bg-[url(https://images.unsplash.com/photo-1615461065624-21b562ee5566?q=80&w=1340&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] ">
-              <div
-                className="flex flex-col items-center space-y-5 
-      backdrop-blur-xl rounded-xl brightness-90 saturate-120 px-10 py-8"
-              >
-                <h1 className=" md:text-5xl text-3xl text-white font-semibold">
-                  Need Emergency Blood?
-                </h1>
-                <p className="text-white ">Find The Donators Near you</p>
-                <div className="flex gap-2.5 md:gap-5">
-                  <Link
-                    className="btn rounded-xl btn-primary btn-lg text-white"
-                    to={"/register"}
-                  >
-                    Request Donation
-                  </Link>
-                </div>
-              </div>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide className=" ">
-            {" "}
             <div className=" flex justify-center items-center w-full h-full bg-cover  bg-[url(https://images.unsplash.com/photo-1536856136534-bb679c52a9aa?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] ">
               <div
                 className="flex flex-col items-center space-y-5 
-      backdrop-blur-xl rounded-xl brightness-90 saturate-120 px-10 py-8"
+      backdrop-blur-md rounded-xl brightness-90 saturate-120 px-10 py-8"
               >
-                <h1 className=" md:text-5xl text-3xl text-white font-semibold">
+                <h1 className=" md:text-5xl  text-3xl text-white font-semibold">
                   Want To Donate Blood?
                 </h1>
                 <p className="text-white ">
